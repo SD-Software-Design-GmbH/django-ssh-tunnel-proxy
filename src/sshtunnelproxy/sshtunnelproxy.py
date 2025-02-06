12c9b73d2b433e07cccd57e8c85d65389667e700
Julian Bürklin <jb@software-design.de>
2025-02-06 07:33:09 +0100

    use sshtunnelproxy.settings

--- src/sshtunnelproxy/sshtunnelproxy.py
+++ src/sshtunnelproxy/sshtunnelproxy.py
@@ -2,7 +2,8 @@ import random
 from contextlib import contextmanager
 
 import pexpect
-from django.conf import settings
+
+from sshtunnelproxy import settings
 
 
 @contextmanager
@@ -10,18 +11,18 @@ def tunnel(remote_port: int = 0, local_port: int = 8000):
     """
     Create an SSH tunnel to a remote server
     """
-    remote_port = remote_port or random.randint(*settings.SSH_TUNNEL_PORT_RANGE)  # noqa: S311
+    remote_port = remote_port or random.randint(*settings.PORT_RANGE)  # noqa: S311
     ssh_tunnel = pexpect.spawn(
-        f"ssh -NtR {remote_port}:localhost:{local_port} {settings.SSH_TUNNEL_USER}@{settings.SSH_TUNNEL_HOST}",
+        f"ssh -NtR {remote_port}:localhost:{local_port} {settings.USER}@{settings.HOST}",
     )
     ssh_tunnel.expect("password:")
-    ssh_tunnel.sendline(settings.SSH_TUNNEL_PASSWORD)
+    ssh_tunnel.sendline(settings.PASSWORD)
 
     # We need to expect something to keep the tunnel open
     # But expect("") doesn't work for some reason
     ssh_tunnel.expect(pexpect.TIMEOUT, timeout=1)
 
     try:
-        yield settings.SSH_TUNNEL_URL_GENERATOR(remote_port)
+        yield settings.URL_GENERATOR(remote_port)
     finally:
         ssh_tunnel.close()
